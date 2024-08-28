b5f4ce4b7dcd1276a173867bada292891daf0d9b
yyyr-otz <32903075+yyyr-otz@users.noreply.github.com>
2024-08-29 02:41:34 +0800

    Update Header.vue

--- components/layouts/Header.vue
+++ components/layouts/Header.vue
@@ -65,7 +65,7 @@ const showMenu = ref(false)
                 title="Sink Dashboard"
               >Dashboard</a>
               <a
-                href="https://github.com/ccbikai/sink"
+                href="https://github.com/yyyr-otz/Sink"
                 target="_blank"
                 title="Github"
                 class="inline-flex items-center w-full px-6 py-3 text-sm font-medium leading-4 text-white bg-gray-900 md:px-3 md:w-auto md:rounded-full hover:bg-gray-800 focus:outline-none md:focus:ring-2 focus:ring-0 focus:ring-offset-2 focus:ring-gray-800"
